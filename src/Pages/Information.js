4314852c42e48c78bc89b033f56cb488344c01d6
yonmoyonmo <yonmosound@gmail.com>
2021-09-20 21:21:36 +0900

    release 1.0

--- src/Pages/Information.js
+++ src/Pages/Information.js
@@ -9,7 +9,15 @@ const Information = () => {
       <Header></Header>
       <div className="container">
         <div className="section">
-          <p>information</p>
+          <div className="info">
+            <div className="baggerWonmo">
+              <img src="/images/baggerWonmo.png"></img>
+            </div>
+            <p>멋들어진 문장 준비중...</p>
+          </div>
+        </div>
+        <div className="section">
+          <h2>wonmonae@gmail.com</h2>
         </div>
       </div>
       <Footer></Footer>
